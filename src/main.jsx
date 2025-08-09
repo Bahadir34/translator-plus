5e81c02c76ddfff29dfc361508a79addb0f5f932
Bahadir34 <bahadirahmetu@gmail.com>
2025-08-10 00:06:06 +0300

    Arranged vercel-analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import "./index.css";
 import App from "./App.jsx";
 import store from "./redux/store.js";
 import { Provider } from "react-redux";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
